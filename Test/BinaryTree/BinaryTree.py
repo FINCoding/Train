5872830f2fae01f50bdf0c835215b433f166ee1b
Ilnar <chief-ilnar.nasibullin@yandex.ru>
2018-07-08 16:46:02 +0300

    'BinaryTree'

---
+++ Test/BinaryTree/BinaryTree.py
@@ -0,0 +1,10 @@
+class BinaryTreeNode:
+    count = 0
+    def add(self, value):
+        if head:
+            addTo(head, value)
+        else:
+            head = BinaryTreeNode(value)
+    
+    def addTo(self, node, value):
+        
