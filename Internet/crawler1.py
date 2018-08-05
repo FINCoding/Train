40c02047aa049cfaf10910890e23db8b48aa1769
Ilnar <chief-ilnar.nasibullin@yandex.ru>
2018-08-05 23:37:33 +0300

    'HomeWork050818'

---
+++ Internet/crawler1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+browser = webdriver.Chrome('C:\Program Files\chromedriver\chromedriver.exe')
+browser.get('http://quotes.toscrape.com/')
+
+elements = browser.find_elements_by_class_name('text')
+for element in elements:
+    text = element.text
+    print(text)
